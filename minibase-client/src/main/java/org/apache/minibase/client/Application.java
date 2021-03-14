902011eda46beedb9373986b2f4105dd8374a7c9
roachxiao <roachxiao@tencent.com>
2021-03-14 13:38:49 +0800

    reconstruct project directory.

---
+++ minibase-client/src/main/java/org/apache/minibase/client/Application.java
@@ -0,0 +1,8 @@
+package org.apache.minibase.client;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ minibase-server/src/main/java/org/apache/minibase/server/Application.java
@@ -0,0 +1,25 @@
+package org.apache.minibase.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application extends SpringBootServletInitializer{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
